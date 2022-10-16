8c1dfdd03ede41c5810c69a731bb941b4bd3f713
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2022-10-15 21:54:01 -0400

    Added check if user actually uses mod orders
    If 2_Individual_Mods is empty, this script will be skipped as a part of the build process.
    Additionally, 2_Modpack will be emptied as a part of rebuilding the modpack from 2_Individual_Mods.

--- 2_Build_Modpack.bat
+++ 2_Build_Modpack.bat
@@ -3,6 +3,17 @@ setlocal enabledelayedexpansion
 for /f "usebackq delims=" %%A in ("0_Variables.txt") do %%A
 
 for /d %%f in ("%modPath%\*") do (
+	goto mods
+)
+goto nomods
+
+:mods
+del /s /q "%modpackPath%\"
+for /d %%i in ("%modpackPath%\*") do (
+	rmdir /s /q "%%i"
+)
+
+for /d %%f in ("%modPath%\*") do (
 	for /f "delims=_" %%n in ("%%~nf") do (
 		set "number=00000%%n"
 		set "file[!number:~-6!]=%modPath%\%%~nf"
@@ -13,4 +24,5 @@ for /f "tokens=2 delims==" %%i in ('set file[') do (
 	ROBOCOPY "%%~fi" %modpackPath% /E
 )
 
+:nomods
 endlocal
